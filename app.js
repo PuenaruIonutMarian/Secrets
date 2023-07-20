d176ad5f0168a68a7047ad83a678db867f4d4359
Pue90 <43031720+Pue90@users.noreply.github.com>
2023-07-20 19:08:22 +0200

    Salting and Hashing implementation

--- app.js
+++ app.js
@@ -1,81 +1,104 @@
 //jshint esversion:6
-require('dotenv').config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+require('dotenv').config(); // Load environment variables from .env file
+const express = require("express"); // Import the Express framework
+const bodyParser = require("body-parser"); // Middleware for parsing request bodies
+const ejs = require("ejs"); // Templating engine for rendering dynamic content
+const mongoose = require("mongoose"); // MongoDB object modeling tool
+// const encrypt = require("mongoose-encryption"); // Encryption plugin for Mongoose
+// const md5 = require("md5"); // Hashing algorithm for password hashing
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const app = express();
+const app = express(); // Create an Express application
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public")); // Serve static files from the "public" directory
+app.set('view engine', 'ejs'); // Set EJS as the view engine for rendering templates
+app.use(bodyParser.urlencoded({extended:true})); // Enable URL-encoded body parsing
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true}); // Connect to the MongoDB database
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: {
+        type: String,
+        required: true
+    }
 });
 
+// This will encrypt only the password field with the use of a secret, which is basically a long string.
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
-//This will encrypt only you password field with the use of secret const wich is bassically a long string. 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+const User = new mongoose.model("User", userSchema); // Create a User model based on the userSchema
 
-const User = new mongoose.model("User", userSchema);
-
-
-
+// Home route handler
 app.get("/", function (req, res){
-    res.render("home");
+    res.render("home"); // Render the "home" template
 });
 
+// Login page route handler
 app.get("/login", function (req, res){
-    res.render("login");
+    res.render("login"); // Render the "login" template
 });
 
+// Login form submission route handler
 app.post("/login", async function (req, res) {
-    try {
-        const username = req.body.username;
-        const password = req.body.password;
-        const foundUser = await User.findOne({ email: username }).exec();
-        if (foundUser && foundUser.password === password) {
-            res.render("secrets");
-        }
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-app.get("/register", function (req, res){
-    res.render("register");
-});
-
-app.post("/register", async function (req, res) {
-    const { username, password } = req.body;
-
-    try {
-        const existingUser = await User.findOne({ email: username }).exec();
-        if (existingUser) {
-            res.send("User already exists.");
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    const foundUser = await User.findOne({ email: username }).exec(); // Find a user with the provided username
+    if (foundUser) {
+      bcrypt.compare(password, foundUser.password, function (err, result) {
+        if (result === true) {
+          res.render("secrets"); // Render the "secrets" template if the passwords match
         } else {
-            const newUser = new User({
-                email: username,
-                password: password
-            });
-            await newUser.save();
-            res.render("secrets");
+          res.send("Invalid password"); // Send an error message if the passwords don't match
         }
-    } catch (err) {
-        console.log(err);
-        res.send("An error occurred during registration.");
+      });
+    } else {
+      res.send("User not found"); // Send an error message if the user is not found
     }
+  } catch (err) {
+    console.log(err);
+    res.send("An error occurred during login."); // Send an error message if an error occurs
+  }
 });
 
 
+// Register page route handler
+app.get("/register", function (req, res){
+    res.render("register"); // Render the "register" template
+});
+
+// Register form submission route handler
+app.post("/register", async function (req, res) {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email: username }).exec(); // Check if the user already exists
+    if (existingUser) {
+      res.send("User already exists."); // Send an error message if the user already exists
+    } else {
+      bcrypt.hash(password, saltRounds, async function (err, hash) {
+        if (err) {
+          console.log(err);
+          res.send("An error occurred during registration.");
+        } else {
+          const newUser = new User({
+            email: username,
+            password: hash, // Store the hashed password in the database
+          });
+          await newUser.save(); // Save the new user to the database
+          res.render("secrets"); // Render the "secrets" template after successful registration
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send("An error occurred during registration."); // Send an error message if an error occurs
+  }
+});
 
 
-
-
-app.listen(3000, function(){console.log("Server started on port 3000.");});
+app.listen(3000, function(){
+    console.log("Server started on port 3000."); // Start the server and listen on port 3000
+});
